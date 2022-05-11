b3bfd3801bdc0f544064bf4acc07be43c06099f9
lelang <admin@cloudcrane.cn>
2022-05-11 23:17:54 +0800

    chore: ci

--- pages/app.ts
+++ pages/app.ts
@@ -4,5 +4,6 @@ import '~/packages/plugin-ui/skin/dark.scss'
 import '~/packages/ketcher-react/index.css'
 import '~/styles/custom-molstar.css'
 import '~/styles/custom-ketcher.css'
+
 import { createViewer } from '~/containers/Viewer'
 createViewer('app')
